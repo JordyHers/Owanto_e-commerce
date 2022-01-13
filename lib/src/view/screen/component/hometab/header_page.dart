4ce7d2c301fd49bbbe485544ecd500e5d7d3a79d
jordyhers <jordy925@gmail.com>
2022-01-13 23:57:59 +0300

    Tunique section added

--- lib/src/view/screen/component/hometab/header_page.dart
+++ lib/src/view/screen/component/hometab/header_page.dart
@@ -1,5 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:owanto_app/src/viewmodel/product_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 class HeaderPage extends StatelessWidget {
   List<String> listImage = [
@@ -10,6 +12,7 @@ class HeaderPage extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
+    ProductViewModel prductVM = Provider.of(context, listen: false);
     return CarouselSlider(
       options: CarouselOptions(
         height: 400,
@@ -20,16 +23,16 @@ class HeaderPage extends StatelessWidget {
 
         // autoPlay: false,
       ),
-      items: listImage
-          .map((e) => Builder(
+      items: prductVM.listProduct!
+          .map((product) => Builder(
                 builder: (BuildContext context) => Container(
                     height: 400,
                     width: 500,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(20)),
                     ),
-                    child: Image.network(
-                      e,
+                    child: Image.asset(
+                      product.urlImage!.first,
                       fit: BoxFit.fill,
                     )),
               ))
--- lib/src/view/screen/favorite_tab.dart
+++ lib/src/view/screen/favorite_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:owanto_app/src/view/screen/component/hometab/header_page.dart';
 import 'package:owanto_app/src/viewmodel/product_viewmodel.dart';
 import 'package:provider/provider.dart';
 
@@ -6,15 +7,45 @@ class FavoriteTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var productProvider = Provider.of<ProductViewModel>(context, listen: false);
-    productProvider.listProduct!.forEach((element) {
-      print(element.isLike);
-    });
+    // productProvider.listProduct!.forEach((element) {
+    //   print(element.isLike);
+    // });
     return SafeArea(
       child: Center(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text("Aucun produit favorisé "), Icon(Icons.info_rounded)],
-      )),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          HeaderPage(),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Aucun produit favorisé "),
+              Icon(Icons.info_rounded)
+            ],
+          ),
+          Center(
+            child: Padding(
+              padding:
+                  EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 60),
+              child: Text(
+                "Vous pouvez faoriser une tenue en cliquant sur le bouton j'aime ",
+                style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print("DEBUG: Favorite TAB Button presssed");
+              },
+              child: Text("Retour à la page d'accueil"),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  textStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300)))
+        ]),
+      ),
     );
   }
 }
